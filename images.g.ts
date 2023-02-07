83d093603671e352f0cce6da0425393476fe948b
vbk46 <123964881+vbk46@users.noreply.github.com>
2023-02-07 08:51:25 +0100

    Update pxt.json, main.blocks, main.py, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,29 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`00780004080200`;
+            case "song2":
+            case "mySong0":return hex`00780004080200`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
